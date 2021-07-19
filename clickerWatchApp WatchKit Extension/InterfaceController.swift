160c6c3d431f4c380bfa1a7d22f0f0963ca12ff2
Javier Perez <javier_perez95@icloud.com>
2021-07-19 05:18:37 -0500

    1st

--- clickerWatchApp WatchKit Extension/InterfaceController.swift
+++ clickerWatchApp WatchKit Extension/InterfaceController.swift
@@ -10,13 +10,42 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    var clicker: Int = 0
+    
+    @IBOutlet weak var reps: WKInterfaceLabel!
+    
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    
+    
+    
+    
 
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
+    @IBAction func clickerTap() {
+        
+    clicker = clicker + 1
+        reps.setText("Repetitions: \(clicker)")
+        
+       
+        timer.start()
+        
+        
+        
+    }
+    @IBAction func endTimer() {
+        
+        
+        timer.stop()
+    }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        
+       
+        
     }
     
     override func didDeactivate() {
