c161e80539ac362bcfe558c5d0cea9f91a3ba73b
Javier Perez <javier_perez95@icloud.com>
2021-07-19 04:24:22 -0500

    Initial Commit

---
+++ clickerWatchApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  clickerWatchApp WatchKit Extension
+//
+//  Created by Javier Perez on 7/19/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
